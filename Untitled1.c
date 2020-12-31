f02f20a4b8732ed512e5cf179396b8509cd8b295
Roboticol <kaushikguha68@gmail.com>
2020-12-31 10:31:49 +0530

    added calculater title

---
+++ Untitled1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+    printf("Welcome to the Calculater Program Made by Oishik Guha\n");
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+
+    return 0;
+}
+
